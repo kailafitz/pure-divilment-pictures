7c6631b6471dfa97726a33085edb7b9d4ea0279e
kailafitz <mikhailafitzpatrick@yahoo.com>
2023-09-23 15:02:33 +0100

    Changes made to About page

--- src/Pages/About/index.tsx
+++ src/Pages/About/index.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { Helmet } from "react-helmet";
-import { Button, Stack, Typography } from "@mui/material";
+import { Box, Button, Stack, Typography } from "@mui/material";
 import Grid from "@mui/material/Unstable_Grid2/Grid2";
 import { StyledAboutImage } from "./styles";
 import PageEndReel from "../../Components/PageEndReel";
@@ -26,13 +26,15 @@ const About = () => {
         />
       </Helmet>
       <StyledAboutImage></StyledAboutImage>
+
       <Typography
         variant="h4"
         sx={{
-          paddingTop: "10vh",
+          marginTop: { xs: "300px", sm: "500px", lg: "700px" },
+          background: (theme) => theme.palette.white.main,
           fontWeight: 400,
           textAlign: "center",
-          marginBottom: (theme) => theme.spacing(3),
+          padding: (theme) => theme.spacing(3, 0),
           textTransform: "unset",
           span: {
             fontStyle: "italic",
@@ -45,11 +47,20 @@ const About = () => {
           out of <span>pure divilment</span>
         </div>
       </Typography>
-      <Grid container sx={{ justifyContent: "center", position: "relative" }}>
+
+      <Grid
+        container
+        sx={{
+          justifyContent: "center",
+          position: "relative",
+          background: (theme) => theme.palette.white.main,
+        }}
+      >
         <Grid md={8} p={5}>
           <Typography
             variant="body1"
             mb={2}
+            textAlign="justify"
             sx={{
               span: {
                 fontStyle: "italic",
@@ -61,8 +72,8 @@ const About = () => {
             Irish film and television production company. Our purpose is to
             empower and create opportunity, empower and showcase exceptional
             industry talent.
-          </Typography>
-          <Typography variant="body1">
+            <br />
+            <br />
             At the helm of the company are Co-Founders, Claire Mooney and Nell
             Hensey. The pair met in 2019 while studying a Masters in Creative
             Production and Screen Finance at the National Film School IADT.
@@ -71,45 +82,57 @@ const About = () => {
           </Typography>
         </Grid>
       </Grid>
+
+      {/* Quote */}
       <Grid container>
         <Grid
           xs={12}
           md={7}
           p={5}
           sx={{
-            background: (theme) => theme.palette.primary.main,
+            // background: (theme) => theme.palette.primary.main,
+            background: "#0d0c0cc4",
+            backdropFilter: "blur(4px)",
             color: (theme) => theme.palette.white.main,
           }}
         >
           <Typography
             color="white"
             variant="h6"
-            sx={{ fontStyle: "italic", fontWeight: 400 }}
+            fontWeight={100}
+            // sx={{ fontStyle: "italic", fontWeight: 400 }}
             mb={3}
+            textAlign="justify"
           >
             With each new project, we strive for the highest level of creativity
             and technical excellence. Our slate is carefully curated with select
             pieces that focus on compelling characters, new perspectives, and
             demonstrate international appeal.
           </Typography>
-          <Typography color="white" variant="h6" textAlign="right">
+          <Typography
+            color="white"
+            variant="h6"
+            textAlign="right"
+            fontWeight={100}
+          >
             - Claire Mooney
           </Typography>
         </Grid>
       </Grid>
-      <Grid container sx={{ justifyContent: "center", position: "relative" }}>
+
+      <Grid
+        container
+        sx={{
+          justifyContent: "center",
+          position: "relative",
+          background: (theme) => theme.palette.white.main,
+        }}
+      >
         <Grid md={8} p={5}>
-          <Typography variant="body1" mb={2}>
-            We deeply value representation and inclusivity. Community is very
-            important to us. We only work with the best, collaborating with
-            like-minded people on local and international projects. Our slate
-            features bold and exciting work that showcases Irish talent and
-            provides a window for our audiences to experience the world in a new
-            and exciting way.
-          </Typography>
           <Typography
             variant="body1"
             mb={2}
+            textAlign="justify"
             sx={{
               span: {
                 fontStyle: "italic",
@@ -117,6 +140,14 @@ const About = () => {
               },
             }}
           >
+            We deeply value representation and inclusivity. Community is very
+            important to us. We only work with the best, collaborating with
+            like-minded people on local and international projects. Our slate
+            features bold and exciting work that showcases Irish talent and
+            provides a window for our audiences to experience the world in a new
+            and exciting way.
+            <br />
+            <br />
             In 2023, <span>P</span>ure <span>D</span>ivilment Pictures launched
             its highly anticipated debut slate featuring an array of exciting
             projects across television, independent film and short form content.
@@ -125,109 +156,132 @@ const About = () => {
           </Typography>
         </Grid>
       </Grid>
+
+      {/* Quote */}
       <Grid container justifyContent="end">
         <Grid
           xs={12}
           md={7}
           p={5}
-          mb={5}
           sx={{
-            background: (theme) => theme.palette.primary.main,
+            // background: (theme) => theme.palette.primary.main,
+            background: "#0d0c0cc4",
+            backdropFilter: "blur(4px)",
             color: (theme) => theme.palette.white.main,
           }}
         >
           <Typography
             color="white"
             variant="h6"
-            sx={{ fontStyle: "italic", fontWeight: 400 }}
+            fontWeight={100}
+            // sx={{ fontStyle: "italic", fontWeight: 400 }}
             mb={3}
           >
             From literary screen adaptations to original character pieces, our
             goal is to tell important stories. Stories that haven't been told on
             screen before. Work that entertains, captivates and inspires.
           </Typography>
-          <Typography color="white" variant="h6" textAlign="right">
+          <Typography
+            color="white"
+            variant="h6"
+            textAlign="right"
+            fontWeight={100}
+          >
             - Nell Hensey
           </Typography>
         </Grid>
       </Grid>
-      <Typography variant="h6" textAlign="center">
+
+      <Typography
+        variant="h6"
+        textAlign="center"
+        pt={5}
+        sx={{
+          background: (theme) => theme.palette.white.main,
+        }}
+      >
         And we're only getting started.
       </Typography>
 
-      <Stack
-        direction={{ xs: "column", md: "row" }}
-        justifyContent="center"
-        pt={5}
-        pb={9}
+      <Box
         sx={{
-          width: { xs: "fit-content", md: "70%" },
-          margin: "0 auto",
-          display: "flex",
-          justifyContent: "space-around",
+          background: (theme) => theme.palette.white.main,
         }}
       >
-        <Button
-          href="/test/productions"
-          variant="contained"
-          color="dark"
+        <Stack
+          direction={{ xs: "column", md: "row" }}
+          justifyContent="space-around"
+          pt={5}
+          pb={9}
           sx={{
-            p: 2,
-            borderRadius: 0,
-            maxWidth: 200,
-            width: { xs: "100%", md: "-webkit-fill-available" },
-            mb: { xs: 5, md: 0 },
-            "&:hover": {
-              backgroundColor: (theme) => theme.palette.dark.light,
-            },
+            width: { xs: "fit-content", md: "70%" },
+            margin: "0 auto",
+            background: (theme) => theme.palette.white.main,
           }}
         >
-          Go To Productions
-        </Button>
+          <Button
+            href="/test/productions"
+            variant="contained"
+            color="dark"
+            sx={{
+              p: 2,
+              borderRadius: 0,
+              maxWidth: 200,
+              width: { xs: "100%", md: "-webkit-fill-available" },
+              mb: { xs: 5, md: 0 },
+              "&:hover": {
+                backgroundColor: (theme) => theme.palette.dark.light,
+              },
+            }}
+          >
+            Go To Productions
+          </Button>
 
-        <Button
-          href="/test/the-creatives"
-          variant="contained"
-          color="dark"
-          sx={{
-            p: 2,
-            borderRadius: 0,
-            maxWidth: 200,
-            width: { xs: "100%", md: "-webkit-fill-available" },
-            mb: { xs: 5, md: 0 },
-            "&:hover": {
-              backgroundColor: (theme) => theme.palette.dark.light,
-            },
-          }}
-        >
-          Meet our Creatives
-        </Button>
+          <Button
+            href="/test/the-creatives"
+            variant="contained"
+            color="dark"
+            sx={{
+              p: 2,
+              borderRadius: 0,
+              maxWidth: 200,
+              width: { xs: "100%", md: "-webkit-fill-available" },
+              mb: { xs: 5, md: 0 },
+              "&:hover": {
+                backgroundColor: (theme) => theme.palette.dark.light,
+              },
+            }}
+          >
+            Meet our Creatives
+          </Button>
 
-        <Button
-          href="/test/reach-out"
-          variant="contained"
-          color="dark"
-          sx={{
-            p: 2,
-            borderRadius: 0,
-            maxWidth: 200,
-            width: { xs: "100%", md: "-webkit-fill-available" },
-            "&:hover": {
-              backgroundColor: (theme) => theme.palette.dark.light,
-            },
-          }}
-        >
-          Reach Out
-        </Button>
-      </Stack>
-      <PageEndReel
+          <Button
+            href="/test/reach-out"
+            variant="contained"
+            color="dark"
+            sx={{
+              p: 2,
+              borderRadius: 0,
+              maxWidth: 200,
+              width: { xs: "100%", md: "-webkit-fill-available" },
+              "&:hover": {
+                backgroundColor: (theme) => theme.palette.dark.light,
+              },
+            }}
+          >
+            Reach Out
+          </Button>
+        </Stack>
+      </Box>
+
+      {/* <PageEndReel
         images={[
           "/about/image-reel/1.png",
           "/about/image-reel/2.png",
           "/about/image-reel/3.png",
           "/about/image-reel/4.png",
         ]}
-      />
+      /> */}
     </>
   );
 };
